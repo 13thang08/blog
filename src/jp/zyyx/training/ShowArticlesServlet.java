6749894b2df04c55ebea97134ed9ba2e3948d092
Vu Manh Thang <thang@zyyx.jp>
2014-11-20 14:44:34 +0900

    show simple

--- src/jp/zyyx/training/ShowArticlesServlet.java
+++ src/jp/zyyx/training/ShowArticlesServlet.java
@@ -15,41 +15,46 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/show-articles")
 public class ShowArticlesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ShowArticlesServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		ArticleService arcticleService = new FileDataService();
-		int pageIndex = 4;
-		ArticlesList articlesList = arcticleService.showArticles(null, pageIndex);
-		System.out.println(pageIndex);
-		if (articlesList != null) {
-			ArrayList<ArticleBean> beanList = articlesList.getList();
-			for (int i = 0; i < beanList.size(); i++) {
-				System.out.println(beanList.get(i).getId());
-				System.out.println(beanList.get(i).getDate());
-				System.out.println(beanList.get(i).getTitle());
-				System.out.println(beanList.get(i).getContent());
-				System.out.println();
-			}
-			
-		}
+	public ShowArticlesServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String page = request.getParameter("page");
+		int pageIndex;
+		try {
+			pageIndex = Integer.parseInt(page) - 1;
+		} catch(NumberFormatException e) {
+			pageIndex = 0;
+		}
+		if (pageIndex < 0)
+			pageIndex = 0;
+		ArticleService articleService = new FileDataService();
+		ArticlesList articlesList = articleService
+				.showArticles(null, pageIndex);
+		request.setAttribute("articlesList", articlesList);
+		request.getRequestDispatcher("/showArticles.jsp").forward(request,
+				response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
