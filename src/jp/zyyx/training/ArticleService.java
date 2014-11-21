5bd7ad713502e3bd1e454a3366ae6eef9170755a
Vu Manh Thang <thang@zyyx.jp>
2014-11-21 12:21:57 +0900

    add article function in Model fix

--- src/jp/zyyx/training/ArticleService.java
+++ src/jp/zyyx/training/ArticleService.java
@@ -2,7 +2,7 @@ package jp.zyyx.training;
 
 public interface ArticleService {
 	public static final int numArticlesPerPage = 5;
-	public ArticlesList showArticles(String searchText, int page);	
+	public ArticlesList showArticles(String searchText, int page);
+	
 	public boolean addArticle(ArticleBean article);
-
 }
